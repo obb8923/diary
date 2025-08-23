a0051e6d1aec49d0e7aac35f47219850d0218158
obb8923 <obb8923@naver.com>
2025-08-23 16:25:40 +0900

    alias

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset','nativewind/babel'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'], // 별칭 기준 경로
+        alias: {
+          '@': './src', // @ 를 src 폴더로 매핑
+          '@assets': './assets',
+          '@domain': './src/domain',
+          '@components': './src/shared/components',
+          '@constants': './src/shared/constants',
+          '@libs': './src/shared/libs',
+          '@store': './src/shared/store',
+          '@Home': './src/domain/Home',
+          '@Calendar': './src/domain/Calendar',
+          '@Profile': './src/domain/Profile',
+        },
+      },
+    ],
+  ],
 };
