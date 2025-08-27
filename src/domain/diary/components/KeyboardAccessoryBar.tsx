ab7fa1be17d1d043a0587efe473752197b7bf99c
obb8923 <obb8923@naver.com>
2025-08-27 20:33:16 +0900

    .

--- src/domain/diary/components/KeyboardAccessoryBar.tsx
+++ src/domain/diary/components/KeyboardAccessoryBar.tsx
@@ -1,15 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { Platform, View, Keyboard, InputAccessoryView, TouchableOpacity } from 'react-native';
 import Animated, { Layout, ZoomIn, ZoomOut } from 'react-native-reanimated';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Text } from '@components/Text';
 import { useSaveDiaryFlow } from '@libs/hooks/useSaveDiaryFlow';
 import { useAnimationStore } from '@store/animationStore';
 
-export const KeyboardAccessoryBar: React.FC = () => {
-  const [isKeyboardVisible, setIsKeyboardVisible] = useState(false);
+export const KeyboardAccessoryBar= () => {
   const [keyboardHeight, setKeyboardHeight] = useState(0);
-  const insets = useSafeAreaInsets();
   const { charactersCount, canSave, isSaving, save } = useSaveDiaryFlow();
   const [showSave, setShowSave] = useState(canSave);
   const { startClosing, startOpening, startSaveSequence } = useAnimationStore();
@@ -29,29 +26,6 @@ export const KeyboardAccessoryBar: React.FC = () => {
     startSaveSequence(save);
   };
 
-  useEffect(() => {
-    const onShow = (e: any) => {
-      setIsKeyboardVisible(true);
-      setKeyboardHeight(e?.endCoordinates?.height || 0);
-    };
-    const onHide = () => {
-      setIsKeyboardVisible(false);
-      setKeyboardHeight(0);
-    };
-    const subscriptions = Platform.select({
-      ios: [
-        Keyboard.addListener('keyboardWillShow', onShow),
-        Keyboard.addListener('keyboardWillHide', onHide),
-      ],
-      default: [
-        Keyboard.addListener('keyboardDidShow', onShow),
-        Keyboard.addListener('keyboardDidHide', onHide),
-      ],
-    });
-    return () => {
-      subscriptions?.forEach((sub) => sub.remove());
-    };
-  }, []);
 
   const Bar = (
     <View
@@ -68,7 +42,7 @@ export const KeyboardAccessoryBar: React.FC = () => {
           <Text text="키보드 내리기" type="kb2023" className="text-text-black" />
         </TouchableOpacity>
 
-        {showSave ? (
+        {showSave && (
           <Animated.View 
           entering={ZoomIn.springify().stiffness(200).damping(16)} 
           exiting={ZoomOut}   
@@ -87,26 +61,7 @@ export const KeyboardAccessoryBar: React.FC = () => {
               />
             </TouchableOpacity>
           </Animated.View>
-        ) : null}
-
-        {__DEV__ ? (
-          <View className="flex-row items-center">
-            <TouchableOpacity
-              onPress={startClosing}
-              activeOpacity={0.9}
-              className="h-8 px-3 mr-2 rounded-full border border-blue-400 justify-center items-center"
-            >
-              <Text text="닫힘 재생" type="kb2023" className="text-text-blue" />
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={startOpening}
-              activeOpacity={0.9}
-              className="h-8 px-3 rounded-full border border-green-400 justify-center items-center"
-            >
-              <Text text="열림 재생" type="kb2023" className="text-green-600" />
-            </TouchableOpacity>
-          </View>
-        ) : null}
+        )}
       </Animated.View>
     </View>
   );
@@ -115,11 +70,11 @@ export const KeyboardAccessoryBar: React.FC = () => {
     return <InputAccessoryView nativeID={'DiaryAccessory'}>{Bar}</InputAccessoryView>;
   }
 
-  return isKeyboardVisible ? (
+  return  (
     <View pointerEvents="box-none" style={{ position: 'absolute', left: 0, right: 0, bottom: keyboardHeight }}>
       {Bar}
     </View>
-  ) : null;
+  ) ;
 };
 
 export default KeyboardAccessoryBar;
