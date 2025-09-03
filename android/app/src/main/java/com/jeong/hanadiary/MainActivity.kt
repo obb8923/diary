b27639c4ae8bca542710dd4e6dd7ad34fb4af86a
obb8923 <obb8923@naver.com>
2025-09-03 19:00:00 +0900

    안드로이드 패키지명 벼변경

--- android/app/src/main/java/com/jeong/hanadiary/MainActivity.kt
+++ android/app/src/main/java/com/jeong/hanadiary/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.diary
+package com.jeong.hanadiary
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "diary"
+  override fun getMainComponentName(): String = "hanadiary"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- android/app/src/main/java/com/jeong/hanadiary/MainApplication.kt
+++ android/app/src/main/java/com/jeong/hanadiary/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.diary
+package com.jeong.hanadiary
 
 import android.app.Application
 import com.facebook.react.PackageList
