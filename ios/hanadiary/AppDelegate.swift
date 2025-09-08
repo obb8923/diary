249bd13e2e3bfc83906e70795d039501819040c3
obb8923 <obb8923@naver.com>
2025-09-08 12:47:24 +0900

    .

--- ios/hanadiary/AppDelegate.swift
+++ ios/hanadiary/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "diary",
+      withModuleName: "hanadiary",
       in: window,
       launchOptions: launchOptions
     )
